d94165fb2e039ea52b9353e08c5db893d9614134
Sicheng Mu <musicheng520@gmail.com>
2026-02-01 22:34:25 +0000

    Typed function signature.

---
+++ src/02-functions.ts
@@ -0,0 +1,52 @@
+import { Friend } from "./myTypes";
+import { friends } from "./01-basics";
+import { Colleague} from "./myTypes";
+import { colleagues } from "./01-basics";
+
+function older(f: Friend) : string {
+     f.age += 1
+     return `${f.name} is now ${f.age}` 
+}
+
+console.log(older(friends[0]))
+
+function allOlder(friends: Friend[]): string[] {
+  return friends.map((f) => {
+    f.age += 1;
+    return `${f.name} is now ${f.age}`;
+  });
+}
+console.log(allOlder(friends));
+
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+  const result = cs.sort(
+    (c1, c2) => c1.contact.extension - c2.contact.extension
+  );
+  return result[cs.length - 1];
+}
+console.log(highestExtension(colleagues.current));
+
+function addColleague(
+  cs: Colleague[],
+  name: string,
+  department: string,
+  email: string
+): void {
+  const highest = highestExtension(cs).contact.extension;
+
+  const newColleague: Colleague = {
+    name: name,
+    department: department,
+    contact: {
+      email: email,
+      extension: highest + 1,
+    },
+  };
+
+  cs.push(newColleague);
+}
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
