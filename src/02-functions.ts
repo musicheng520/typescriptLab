9394f4a9acff27703abfe7a8195b49a454667168
Sicheng Mu <musicheng520@gmail.com>
2026-02-01 22:48:18 +0000

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -80,3 +80,12 @@ function findFriends(fs: Friend[], predicate: (friend: Friend) => boolean) {
 console.log(findFriends(friends, (friend) => friend.name.startsWith("Pa")));
 console.log(findFriends(friends, (friend) => friend.age < 35));
 
+function addInterest(friend: Friend, interest: string): string[] {
+  if (friend.interests === undefined) {
+    friend.interests = [];
+  }
+
+  friend.interests.push(interest);
+  return friend.interests;
+}
+console.log(addInterest(friends[1], "Politics"));
